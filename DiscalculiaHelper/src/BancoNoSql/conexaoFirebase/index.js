b9cd8a3e8023173858e222000ec88a818720ffae
Hugo Finote <34668254+finotty@users.noreply.github.com>
2026-03-26 11:55:52 -0300

    Clear Firebase configuration values
    Removed sensitive Firebase configuration details.

--- DiscalculiaHelper/src/BancoNoSql/conexaoFirebase/index.js
+++ DiscalculiaHelper/src/BancoNoSql/conexaoFirebase/index.js
@@ -3,14 +3,14 @@ import {getAuth} from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAz4QxxGSuvwunBx8JgFxZWzFjvMlC5Oko",
-    authDomain: "discalculiahelper.firebaseapp.com",
-    databaseURL: "https://discalculiahelper-default-rtdb.firebaseio.com",
-    projectId: "discalculiahelper",
-    storageBucket: "discalculiahelper.appspot.com",
-    messagingSenderId: "841276208621",
-    appId: "1:841276208621:web:4f622ba6eb4413ebaaa3e4",
-    measurementId: "G-979X2T0PBT"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
   
   // Initialize Firebase
